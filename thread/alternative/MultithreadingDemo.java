1acf4895aa1bae15cb59f5ff13c1e3348018f05d
praveen <praveenthogaru13@gmail.com>
2019-10-16 00:10:52 +0530

    Thread program by extending a Thread class

---
+++ thread/alternative/MultithreadingDemo.java
@@ -0,0 +1,35 @@
+// Java code for thread creation by extending 
+// the Thread class 
+class MultithreadingDemo extends Thread 
+{ 
+	public void run() 
+	{ 
+		try
+		{ 
+			// Displaying the thread that is running 
+			System.out.println ("Thread " + 
+				Thread.currentThread().getId() + 
+				" is running"); 
+
+		} 
+		catch (Exception e) 
+		{ 
+			// Throwing an exception 
+			System.out.println ("Exception is caught"); 
+		} 
+	} 
+} 
+
+// Main Class 
+public class Multithread 
+{ 
+	public static void main(String[] args) 
+	{ 
+		int n = 8; // Number of threads 
+		for (int i=0; i<8; i++) 
+		{ 
+			MultithreadingDemo object = new MultithreadingDemo(); 
+			object.start(); 
+		} 
+	} 
+} 
